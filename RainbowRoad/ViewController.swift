3cdb794b361dce3060e998386e71829d89dd5153
grantwbrooks <grantwbrooks@gmail.com>
2017-09-11 21:16:18 -0400

    tweaks

--- RainbowRoad/ViewController.swift
+++ RainbowRoad/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var colors: [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myColor", for: indexPath)
+        cell.backgroundColor = colors[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.rowHeight = 120
     }
 
     override func didReceiveMemoryWarning() {
